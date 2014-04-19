fb1823d365b69a3bf76c738c2b2a3989b24c68c6
Zeno Rocha <zeno.rocha@liferay.com>
2014-04-19 11:04:42 -0300

    Move DocPad's config file to JavaScript

---
+++ docpad.js
@@ -0,0 +1,16 @@
+// DocPad Configuration File
+// http://docpad.org/docs/config
+
+// Define the DocPad Configuration
+var docpadConfig = {
+    templateData: {
+        site: {
+            name: "WebPlatform",
+            title_append: " · WebPlatform.org",
+            tagline: "Your Web, documented"
+        }
+    }
+};
+
+// Export the DocPad Configuration
+module.exports = docpadConfig;
