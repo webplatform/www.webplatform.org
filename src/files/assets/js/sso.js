101e895c5d292ea6624a3cf1ddb0eb9d0c80e016
Renoir Boulanger <renoir@w3.org>
2014-07-24 19:59:31 -0400

    Refactoring library behavior
    * Can declare config before initializing
    * Can post messages manually (temporary?)
    
    Change-Id: Ic0185e09268e1f17f34e6517c7c582c46cd01f56

--- src/files/assets/js/sso.js
+++ src/files/assets/js/sso.js
@@ -1,175 +1,257 @@
-(function ssoInit(w) {
+/*global sso,mw,window,document,XMLHttpRequest,ssoOptions*/
 
-  /**
-   * Articles to enhance support:
-   * - Better IE support:
-   *   - note: IE11 on Windows 8.1 works
-   *   - url: http://www.informit.com/articles/article.aspx?p=667416&seqNum=2
-   * - Better Async with Promise:
-   *   - note: We need only one call so far, maybe overkill
-   *   - url: http://krasimirtsonev.com/blog/article/Cross-browser-handling-of-Ajax-requests-in-absurdjs
-   */
+/**
+ * WebPlatform SSO Relying Party communication manager
+ *
+ *
+ * # Trying manually the API
+ *
+ *   From a browser window that can frame accounts.webplatform.org, run in the
+ *   developer tools terminal.
+ *
+ *   e.g.:
+ *       ```javascript
+ *       var IdP = "https://accounts.webplatform.org/channel?service=sso";
+ *       var tunnel = document.getElementById("authChecker").contentWindow;
+ *
+ *       tunnel.postMessage("ping:Renoirb", IdP); // Hi IdP, i’m signed-in!
+ *       tunnel.postMessage("ping:null", IdP);    // Hi IdP, i’m NOT signed-in, should I be?
+ *       tunnel.postMessage("signoff", IdP);      // Hi IdP, destroy my session, i'll do the same!
+ *       ```
+ *
+ * # Improvement notes:
+ *
+ * - Better IE support:
+ *   - note: IE11 on Windows 8.1 works
+ *   - url: http://www.informit.com/articles/article.aspx?p=667416&seqNum=2
+ * - Better Async with Promise:
+ *   - note: We need only one call so far, maybe overkill
+ *   - url: http://krasimirtsonev.com/blog/article/Cross-browser-handling-of-Ajax-requests-in-absurdjs
+ **/
+(function ssoHandlerClosure(window) {
+    "use strict";
 
-  var redirectTo = encodeURIComponent(window.location.protocol) + "%2F%2F" + window.location.hostname + encodeURIComponent(window.location.pathname||""),
-      searchQuery = (!!window.location.search)?encodeURIComponent(window.location.search||""):"",
-      serviceEndpoint = "https://accounts.webplatform.org/channel",
-      recoverEndpoint = serviceEndpoint + "?service=recover&redirectTo=" + redirectTo + searchQuery,
-      destroyEndpoint = serviceEndpoint + "?service=destroy",
-      callbackUri = "/test/Special:AccountsHandler/callback",
-      received = null,
-      checkedOnce = false,
-      recoveryPayload = "recoveryPayload=",
-      logEvent = function(e){ console.debug(e) },
-      checkedRecentlyCookieName = "wpdSsoUsername",
-      postSuccessStatusValue = 201,
-      postLogoffStatusValue = 401,
-      acceptableActions = {recover: recoverEndpoint, destroy: destroyEndpoint};
-      //wpdSsoUsername = document.cookie.replace(/(?:(?:^|.*;\s*)wpdSsoUsername\s*\=\s*([^;]*).*$)|^.*$/, "$1") || null;
+    var channelOrigin = "https://accounts.webplatform.org/channel?service=sso",
+        wpdSsoUsername = document.cookie.replace(/(?:(?:^|.*;\s*)wpdSsoUsername\s*\=\s*([^;]*).*$)|^.*$/, "$1");
 
-  function iframeLoadedHandler() {
-    setTimeout(sso.doCheck.bind(sso), 400); // Scope hoisting
-    logEvent("iframeLoadedHandler, About to launch the SSO check");
-  }
-
-  function postBackendHandler() {
-    // this, in this function, represents an XHR object
-    if (this.readyState === 4) {
-      if (this.status === postSuccessStatusValue) {
-        forceReload(this, "You are signed on, we are about to reload this page");
-      }
-      if (this.status === postLogoffStatusValue) {
-        forceReload(this, "You are signed off, we are about to reload this page");
-      }
-    }
-  }
-
-  function forceReload(xhrObj, message) {
-    // this, in this function, represent sso
-    try {
-      mw.notify(message);
-    } catch(e) {}
-    logEvent("forceReload; service said status is " + xhrObj.status + " we have to reload!", xhrObj);
-    window.location.reload(true);
-  }
-
-  function createTunnel(action) {
-    var authChecker = null,
-        myEndpoint  = (acceptableActions.hasOwnProperty(action)||false === true)?acceptableActions[action]:null;
-
-    logEvent('createTunnel, might create iframe to ', myEndpoint );
-
-    if (myEndpoint !== null) {
-      authChecker = document.createElement('iframe');
-      authChecker.src = myEndpoint;
-      authChecker.width = 0;
-      authChecker.height = 0;
-      authChecker.id = "authChecker";
-      authChecker.onload = iframeLoadedHandler;
-
-      try {
-        document.body.appendChild(authChecker);
-        document.getElementById("authChecker").style.display = "none";
-      } catch(e) {
-        logEvent("createTunnel, caught exception will return null", e);
-        return null;
-      }
+    function iframeLoadedHandler() {
+        setTimeout(sso.doCheck.bind(sso), 400);
+        console.log("iframeLoadedHandler; iframeLoadedHandler, will call check in a few seconds");
     }
 
-    return authChecker;
-  }
+    function createTunnel() {
+        var authChecker = null,
+            myEndpoint  = sso.config.serviceEndpoint;
 
-  function messageHandler(input) {
-    logEvent("messageHandler, input:", arguments);
-    if ( !! input.data ) {
-      received = input.data;
-      recoveryPayload += received.recoveryPayload;
-      hasSession = received.hasSession || false;
-      logEvent("messageHandler, to processReceived call [hasSession,recoveryPayload,received]:", hasSession, recoveryPayload, received);
-      processReceived(hasSession);
-    } else {
-      logEvent("messageHandler; no data received from accounts server");
-    }
-  }
+        console.log("createTunnel; to create iframe to [myEndpoint]", myEndpoint);
 
-  function processReceived(hasSessionBool) {
-    if ( hasSessionBool === false ) {
-      logEvent("processReceived; No Session, nothing to do");
-      return;
-    }
+        if (myEndpoint === null) {
+            throw new Error("createTunnel; No endpoint to send requests to for, we got " + myEndpoint);
+        }
+        authChecker = document.createElement('iframe');
+        authChecker.src = myEndpoint;
+        authChecker.width = 0;
+        authChecker.height = 0;
+        authChecker.id = "authChecker";
+        authChecker.onload = iframeLoadedHandler;
 
-    logEvent("processReceived; Should send post to " + callbackUri, received, sso.state);
-    postBackend();
-  }
+        try {
+            document.body.appendChild(authChecker);
+            document.getElementById("authChecker").style.display = "none";
+        } catch (e) {
+            console.log("createTunnel, caught exception will return null", e);
+            return null;
+        }
 
-  // Maybe improve as described https://gist.github.com/shimondoodkin/4706967
-
-  function postBackend() {
-    var xhr;
-    if (window.XMLHttpRequest) {
-      xhr = new XMLHttpRequest();
-      xhr.open("POST", callbackUri, true);
-      //xhr.setRequestHeader("Content-length", recoveryPayload.length);
-      //xhr.setRequestHeader("Connection", "close");
-      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-      xhr.onreadystatechange = postBackendHandler.bind(xhr);
-      xhr.send(recoveryPayload);
-    }
-  }
-
-  var SsoHandlerClass = function SsoHandlerClass ( ) {
-      this.state = {
-          username: null,
-          isAnonymous: true,
-          hasSession: false
-      };
-  };
-
-  SsoHandlerClass.prototype.doCheck = function doCheck ( ) {
-    logEvent("doCheck; [callbackUri,isAnonymous,username]: ", callbackUri, this.state.isAnonymous, this.state.username);
-
-    if (self.tunnel === null) {
-      logEvent("doCheck; no tunnel, we stop here.");
-      return;
+        return authChecker;
     }
 
-    logEvent("doCheck; pinging the accounts server");
-    try {
-      self.tunnel.contentWindow.postMessage("ping:" + this.state.username, recoverEndpoint);
-    } catch(e) {}
-  };
+    function postBackendHandler(XMLHttpRequestProgressEvent) {
+        var xhrObj = XMLHttpRequestProgressEvent.currentTarget;
 
-  SsoHandlerClass.prototype.init = function init (setCallbackUri, logging) {
-    var action   = 'recover';
-    console.log("init; this is ", this);
-
-    if ( !! w.ssoPostSuccessStatusValue ) {
-      postSuccessStatusValue = w.ssoPostSuccessStatusValue;
-    }
-    if ( !! w.ssoPostLogoffStatusValue ) {
-      postLogoffStatusValue = w.ssoPostLogoffStatusValue;
-    }
-
-    if ( logging === false ) {
-      logEvent = function(){}; // Doing nothing with it.
+        if (xhrObj.readyState === 4) {
+            console.log("postBackendHandler; readyState=4, status=" + xhrObj.status);
+            console.debug("postBackendHandler; xhrObj=", xhrObj);
+            if (xhrObj.status === sso.config.alreadyHasSessionStatusValue) {
+                return;
+            }
+            if (xhrObj.status === sso.config.postSuccessStatusValue) {
+                window.location.reload(true);
+            }
+            if (xhrObj.status === sso.config.postLogoffStatusValue) {
+                window.location.reload(true);
+            }
+        }
     }
 
-    if ( !! setCallbackUri && typeof setCallbackUri === "string" ) {
-      callbackUri = setCallbackUri;
+    // Maybe improve as described https://gist.github.com/shimondoodkin/4706967
+    function postBackend(stateObj) {
+        var xhr,
+            self = this;
+
+        console.debug("postBackend; stateObj", stateObj);
+        console.debug("postBackend; self", self);
+
+        if (window.XMLHttpRequest) {
+            xhr = new XMLHttpRequest();
+            xhr.open("POST", self.config.callbackUri, true);
+            //xhr.setRequestHeader("Content-length", recoveryPayload.length);
+            //xhr.setRequestHeader("Connection", "close");
+            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            xhr.onreadystatechange = postBackendHandler;
+            xhr.send(stateObj.recoveryPayload);
+        }
     }
 
-    this.state.username = w.mw.user.getName();
-    this.state.isAnonymous = (this.state.username === null)?true:false;
+    function processReceived() {
+        var self = this;
 
-    logEvent("init called [isAnonymous,callbackUri]: ", this.state, callbackUri);
+        console.debug("processReceived; self", self);
 
-    self.tunnel = createTunnel(action); // Defaults to NULL anyway
+        if (self.state.ssoHasSession === true && self.state.isAnonymous === false) {
+            console.log("processReceived; Session on both accounts server and local; nothing else to do");
+            return;
+        }
 
-    logEvent("init, end state is", this.state);
-  };
+        if (self.state.ssoHasSession === false && self.state.isAnonymous === true) {
+            console.log("processReceived; No Session on accounts server, no session locally; nothing to do");
+            return;
+        }
 
-  w.sso = new SsoHandlerClass();
+        console.log("processReceived; Should send post to " + self.config.callbackUri);
+        postBackend.call(self, self.state);
+    }
 
-  w.addEventListener("message", messageHandler, false);
+    function messageHandler(input) {
+        var received,
+            self = this;
 
-  setTimeout(sso.init.bind(w.sso), 800);
+        if (!!input.data) {
+            received = input.data;
+            self.state.recoveryPayload += received.recoveryPayload || "";
+            self.state.ssoHasSession = received.hasSession || false; // Remains false if its not there
+            console.log("messageHandler, to processReceived call [ssoHasSession]:", self.state.ssoHasSession);
+            console.debug("messageHandler, to processReceived call [recoveryPayload,received]:", self.state.recoveryPayload, received);
+            processReceived.call(self);
+        } else {
+            console.log("messageHandler; no data received from accounts server");
+        }
+    }
+
+    /** @namespace */
+    function SsoHandlerClass(configObject) {
+
+        if (window.ssoOptions.logging === false) {
+            console = console || {}; // jshint ignore:line
+            console.debug = function () {}; // jshint ignore:line
+        }
+
+        var defaultConfigs = {
+                serviceEndpoint: channelOrigin,
+                callbackUri: "/test/Special:AccountsHandler/callback",
+                postSuccessStatusValue: 201,
+                postLogoffStatusValue: 401,
+                alreadyHasSessionStatusValue: 204,
+                logging: false,
+                appendRefererServiceEndpoint: false
+            },
+            mergedConfig = {},
+            prop,
+            inputConfig = configObject || {};
+
+        for (prop in defaultConfigs) {
+            if (Object.prototype.hasOwnProperty.call(defaultConfigs, prop)) {
+                mergedConfig[prop] = defaultConfigs[prop];
+            }
+        }
+        for (prop in inputConfig) {
+            if (Object.prototype.hasOwnProperty.call(inputConfig, prop)) {
+                mergedConfig[prop] = inputConfig[prop];
+            }
+        }
+
+        this.config = mergedConfig;
+
+        this.state = {
+            received: null,
+            checkedOnce: false,
+            ssoHasSession: false,
+            recoveryPayload: "recoveryPayload=",
+            username: null,
+            isAnonymous: true
+        };
+
+        window.addEventListener("message", messageHandler.bind(this), false);
+
+        console.debug("SsoHandlerClass instantiation; [wpdSsoUsername]:", wpdSsoUsername);
+    }
+
+    SsoHandlerClass.prototype.doCheck = function ssoHandlerDoCheck() {
+        var self = this;
+        console.debug("doCheck; [callbackUri,isAnonymous,username]: ", self.config.callbackUri, self.state.isAnonymous, self.state.username);
+
+        if (self.tunnel === null) {
+            console.log("doCheck; no tunnel, we stop here.");
+            return;
+        }
+
+        console.debug("doCheck; pinging the accounts server");
+        try {
+            self.tunnel.contentWindow.postMessage("ping:" + self.state.username, self.config.serviceEndpoint);
+        } catch (ignore) {}
+    };
+
+    SsoHandlerClass.prototype.signOut = function ssoHandlerSignOut() {
+        var self = this;
+        console.log("signOut;");
+
+        if (self.tunnel === null) {
+            console.log("signOut; no tunnel, we stop here.");
+            return;
+        }
+
+        console.log("signOut; asking to destroy the session");
+        try {
+            self.tunnel.contentWindow.postMessage("signout:" + self.state.username, self.config.serviceEndpoint);
+        } catch (ignore) {}
+    };
+
+    SsoHandlerClass.prototype.post = function tempPost(intentId) {
+        var self = this;
+        console.debug("post; with intent id " + intentId);
+
+        if (self.tunnel === null) {
+            console.log("post; no tunnel, we stop here.");
+            return;
+        }
+
+        try {
+            self.tunnel.contentWindow.postMessage(intentId, self.config.serviceEndpoint);
+        } catch (ignore) {}
+    };
+
+    SsoHandlerClass.prototype.init = function ssoHandlerInit() {
+        var self   = this;
+        self.state.username = window.mw.user.getName() || null;
+        self.state.isAnonymous = (self.state.username === null) ? true : false;
+
+        if (self.config.appendRefererServiceEndpoint === true) {
+            var redirectTo = encodeURIComponent(window.location.protocol) + "%2F%2F" + window.location.hostname + encodeURIComponent(window.location.pathname || ""),
+                searchQuery = (!!window.location.search) ? encodeURIComponent(window.location.search || "") : "";
+
+            self.config.serviceEndpoint += "&redirectTo=" + redirectTo + searchQuery;
+        }
+
+        self.tunnel = createTunnel();
+        try {
+            window.mw.notify("Syncing with central account server, the page might reload.", {title: "WebPlatform SSO"});
+        } catch (ignore) {}
+        console.debug("init; state:", self.state);
+    };
+
+    var ssoOptions = window.ssoOptions || {};
+    window.sso = new SsoHandlerClass(ssoOptions);
+
+    setTimeout(sso.init.bind(sso), 800);
+
 })(window);
